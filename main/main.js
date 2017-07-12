f920a3fe7926fc34bc1ed27e27117d1fae159674
WuYiChenhml <1055200502@qq.com>
2017-07-12 09:24:36 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -5,7 +5,7 @@ module.exports = function main() {
   var result;
   for(var i = length - 1; i < 0; i--)
   {
-    result =  Math.floor(num / Math.pow(10,i));
+    result = result + Math.floor(num / Math.pow(10,i));
     num = num - Math.floor(num / Math.pow(10,i)) * Math.pow(10,i);
   }
 return result;
